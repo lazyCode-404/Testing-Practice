d01745e0c9f1cb963180e0d06cb25584eda9ea36
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-06-16 19:04:42 -0400

    Add firstTask.js

---
+++ firstTask.js
@@ -0,0 +1,8 @@
+const stringLength = (string) => {
+    if (string.length >= 1 && string.length <= 10) {
+      return string.length;
+    }
+    throw new Error('String must have at least 1 character and not more than 10');
+  };
+  
+  module.exports = stringLength;
