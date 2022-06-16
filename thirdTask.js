28b07ac75b078caf2634acedf84401d12a871c51
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-06-16 19:08:14 -0400

    Add thirdTask.js

---
+++ thirdTask.js
@@ -0,0 +1,22 @@
+class Calculator {
+    constructor() {}
+  
+    add(a, b) {
+      return a + b;
+    }
+  
+    subtract(a, b) {
+      return a - b;
+    }
+  
+    divide(a, b) {
+      return a / b;
+    }
+  
+    multiply(a, b) {
+      return a * b;
+    }
+  }
+  
+  const calculator = new Calculator();
+  module.exports = calculator;
